f4ef1d44fdab65a78430407ecff9ae9524662353
Ivo Bellin Salarin <nilleb@users.noreply.github.com>
2022-05-05 09:35:22 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='voltalis-cli',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/nilleb/voltalis-cli',
     author='Ivo Bellin Salarin',
     author_email='ivo@nilleb.com',
